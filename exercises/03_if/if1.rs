e2b18fe9b9fb6109079064afdca17c47e671e9e0
geekysilento <125768789+geekysilento@users.noreply.github.com>
2024-06-28 19:43:48 +0000

    milestone: completed exercises till Chapter 5 - Primitive Types

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -2,7 +2,7 @@
 //
 // Execute `rustlings hint if1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::cmp::max;
 
 pub fn bigger(a: i32, b: i32) -> i32 {
     // Complete this function to return the bigger number!
@@ -10,6 +10,8 @@ pub fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+
+    max(a, b)
 }
 
 // Don't mind this for now :)
